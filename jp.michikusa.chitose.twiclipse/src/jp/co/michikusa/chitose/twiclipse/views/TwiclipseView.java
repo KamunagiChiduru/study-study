13d6f17a37d51f335fc93fd51aeaf4f560a0240c
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2013-09-17 06:39:45 +0900

    2 changes below
    * correct plug-in category, plug-in name
    * clean meaningless codes

--- jp.michikusa.chitose.twiclipse/src/jp/co/michikusa/chitose/twiclipse/views/TwiclipseView.java
+++ jp.michikusa.chitose.twiclipse/src/jp/co/michikusa/chitose/twiclipse/views/TwiclipseView.java
@@ -1,36 +1,15 @@
 package jp.co.michikusa.chitose.twiclipse.views;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.part.ViewPart;
-
-import com.google.common.base.Joiner;
 
 /**
  * This sample class demonstrates how to plug-in a new
@@ -47,70 +26,16 @@ import com.google.common.base.Joiner;
  * <p>
  */
 
-public class TwiclipseView extends ViewPart implements SelectionListener
+public class TwiclipseView extends ViewPart
 {
     /**
      * The ID of the view as specified by the extension.
      */
-    public static final String ID= "jp.co.michikusa.chitose.twiclipse.views.SampleView";
+    public static final String ID= "jp.co.michikusa.chitose.twiclipse.views.TwiclipseView";
 
-    private Action action1;
-    private Action action2;
-    private Action doubleClickAction;
+    private Button syncronizeButton;
 
-    @Override
-    public void widgetDefaultSelected(SelectionEvent e)
-    {
-    }
-
-    @Override
-    public void widgetSelected(SelectionEvent event)
-    {
-        try
-        {
-            final String source= Joiner.on("\n").join( //
-                    Files.readAllLines( //
-                            new File("C:/Users/USER1/Documents/sources/java/src/jp/michikusa/chitose/Simple.java").toPath(), //
-                            Charset.forName("UTF-8") //
-                    ));
-            System.out.println("<!-- ===== START ===== -->");
-
-            final ASTParser parser= ASTParser.newParser(AST.JLS4);
-
-            parser.setResolveBindings(true);
-            parser.setSource(source.toCharArray());
-
-            final ASTNode node= parser.createAST(null);
-
-            node.accept(new ASTVisitor()
-            {
-                @Override
-                public boolean visit(MethodDeclaration node)
-                {
-                    System.out.println("### MethodDeclaration: " + node);
-
-                    node.accept(new ASTVisitor()
-                    {
-                        @Override
-                        public boolean visit(VariableDeclarationStatement node)
-                        {
-                            System.out.println("### VariableDeclarationStatement: " + node);
-
-                            return false;
-                        }
-                    });
-
-                    return true;
-                }
-            });
-
-            System.out.println("<!-- ===== END ===== -->");
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+    private TableViewer tweetsViewer;
 
     /**
      * This is a callback that will allow us
@@ -127,9 +52,10 @@ public class TwiclipseView extends ViewPart implements SelectionListener
 
         final ScrolledComposite base= new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
 
-        base.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        base.setExpandVertical(false);
-        base.setExpandHorizontal(false);
+        base.setLayout(GridLayoutFactory.fillDefaults().create());
+        base.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+        base.setExpandVertical(true);
+        base.setExpandHorizontal(true);
 
         final Composite content= this.newContent(base);
 
@@ -141,127 +67,43 @@ public class TwiclipseView extends ViewPart implements SelectionListener
 
         content.setSize(point);
         base.setMinSize(point);
-
-        // makeActions();
-        // hookContextMenu();
-        // hookDoubleClickAction();
-        // contributeToActionBars();
     }
 
     private Composite newContent(Composite parent)
     {
         final Composite base= new Composite(parent, SWT.NONE);
 
-        base.setLayout(GridLayoutFactory.swtDefaults().create());
+        base.setLayout(GridLayoutFactory.fillDefaults().create());
 
         {
-            final Button reflesh= new Button(base, SWT.PUSH);
+            this.syncronizeButton= new Button(base, SWT.PUSH);
 
-            reflesh.setText("syncronize");
-            reflesh.addSelectionListener(this);
+            this.syncronizeButton.setText("syncronize");
         }
         {
-            final TableViewer viewer= new TableViewer(base);
+            this.tweetsViewer= new TableViewer(base);
 
-            final TableViewerColumn column= new TableViewerColumn(viewer, SWT.NONE);
+            this.tweetsViewer.getTable().setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+            this.tweetsViewer.getTable().setHeaderVisible(true);
+            this.tweetsViewer.getTable().setLinesVisible(true);
 
-            column.getColumn().setText("user");
+            TableViewerColumn column;
+
+            column= new TableViewerColumn(this.tweetsViewer, SWT.LEFT);
+            column.getColumn().setText("user name");
+            column.getColumn().pack();
+
+            column= new TableViewerColumn(this.tweetsViewer, SWT.LEFT);
+            column.getColumn().setText("tweet");
+            column.getColumn().pack();
+
+            column= new TableViewerColumn(this.tweetsViewer, SWT.CENTER);
+            column.getColumn().setText("posted at");
+            column.getColumn().pack();
         }
 
         return base;
     }
-
-    private void hookContextMenu()
-    {
-        // final MenuManager menuMgr= new MenuManager("#PopupMenu");
-        //
-        // menuMgr.setRemoveAllWhenShown(true);
-        // menuMgr.addMenuListener(new IMenuListener(){
-        // @Override
-        // public void menuAboutToShow(IMenuManager manager){
-        // SampleView.this.fillContextMenu(manager);
-        // }
-        // });
-        //
-        // final Menu menu= menuMgr.createContextMenu(viewer.getControl());
-        // viewer.getControl().setMenu(menu);
-        // getSite().registerContextMenu(menuMgr, viewer);
-    }
-
-    private void contributeToActionBars()
-    {
-        IActionBars bars= getViewSite().getActionBars();
-        fillLocalPullDown(bars.getMenuManager());
-        fillLocalToolBar(bars.getToolBarManager());
-    }
-
-    private void fillLocalPullDown(IMenuManager manager)
-    {
-        manager.add(action1);
-        manager.add(new Separator());
-        manager.add(action2);
-    }
-
-    // private void fillContextMenu(IMenuManager manager){
-    // manager.add(action1);
-    // manager.add(action2);
-    // // Other plug-ins can contribute there actions here
-    // manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-    // }
-
-    private void fillLocalToolBar(IToolBarManager manager)
-    {
-        manager.add(action1);
-        manager.add(action2);
-    }
-
-    // private void makeActions(){
-    // action1= new Action(){
-    // @Override
-    // public void run(){
-    // showMessage("Action 1 executed");
-    // }
-    // };
-    // action1.setText("Action 1");
-    // action1.setToolTipText("Action 1 tooltip");
-    // action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-    // getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-    //
-    // action2= new Action(){
-    // @Override
-    // public void run(){
-    // showMessage("Action 2 executed");
-    // }
-    // };
-    // action2.setText("Action 2");
-    // action2.setToolTipText("Action 2 tooltip");
-    // action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-    // getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-    // doubleClickAction= new Action(){
-    // @Override
-    // public void run(){
-    // ISelection selection= viewer.getSelection();
-    // Object obj= ((IStructuredSelection)selection).getFirstElement();
-    // showMessage("Double-click detected on " + obj.toString());
-    // }
-    // };
-    // }
-
-    // private void hookDoubleClickAction(){
-    // viewer.addDoubleClickListener(new IDoubleClickListener(){
-    // @Override
-    // public void doubleClick(DoubleClickEvent event){
-    // doubleClickAction.run();
-    // }
-    // });
-    // }
-
-    // private void showMessage(String message){
-    // MessageDialog.openInformation(
-    // viewer.getControl().getShell(),
-    // "Sample View",
-    // message);
-    // }
 
     /**
      * Passing the focus request to the viewer's control.
@@ -269,6 +111,6 @@ public class TwiclipseView extends ViewPart implements SelectionListener
     @Override
     public void setFocus()
     {
-        // viewer.getControl().setFocus();
+        this.tweetsViewer.getControl().setFocus();
     }
 }
